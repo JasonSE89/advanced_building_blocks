6eba3c5198246d7c0e87f600318a703f04ca500f
Jason <jason.epstein89@gmail.com>
2017-03-26 12:49:14 -0400

    bubble_sort method completed

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,22 +3,19 @@ user_input = gets.chomp.split(',')
 user_input = user_input.collect{|element| element.to_i}
 bubble_list = user_input.collect{|element| element.to_i}
 def swap(cur,bubble_list)
-	temp = bubble_list[cur]
-	bubble_list[cur]=bubble_list[cur+1]
-	bubble_list[cur+1] = temp
+	bubble_list[cur],bubble_list[cur+1] = bubble_list[cur+1],bubble_list[cur]
+	return bubble_list
 end
 def bubblesort(bubble_list)
-	total_passes = 0
-	cur = 0
-	while total_passes < bubble_list.length 
+	(bubble_list.length-1).times do
+		cur = 0
 		while cur < bubble_list.length-1
-			if bubble_list[cur] > bubble_list[cur+1]  
+			if (bubble_list[cur] <=> bubble_list[cur+1]) == 1  
 				swap(cur,bubble_list)
 			end
 			cur+=1
 		end
-		total_passes += 1
-	end 
+	end
 	print bubble_list
 	puts "\n"
 end
