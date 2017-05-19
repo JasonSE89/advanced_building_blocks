ec4f29c2188e232bee0032e9a10b1b982b65198c
Jason <jason.epstein89@gmail.com>
2017-05-19 14:19:57 -0400

    updated enumeration_methods.rb

--- enumeration_methods.rb
+++ enumeration_methods.rb
@@ -6,4 +6,11 @@ module Enumerable
       	i+=1
       end
 	end
+	def my_each_with_index
+		i = 0
+		while(i < self.length) do
+		  yield(self.keys[i], self[self.keys[i]])
+		  i+=1
+		end
+	end
 end
